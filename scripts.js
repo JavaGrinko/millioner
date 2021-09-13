aa4a1c27ff0d4b7d512a9131604dd9de0da2aadf
Artem Grinko <asgrinko@alfabank.ru>
2021-09-13 17:29:41 +0300

    заготовка для вопросов

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const questions = [{
+    question: 'Сколько будет 2+2?',
+    answers: ['0', '1', '3', '4'],
+    correctAnswerIndex: 3,
+    difficalty: 0
+}];
+
+window.onload = () => {
+    
+}
